dc1e011ffe9cd873120a0c502d9e6da47a58b3f4
Colin Aurélien (IMTAtlantique) <acolin@groupcls.com>
2024-07-15 11:28:27 +0200

    Fix required non-py files.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = []
 setup(
     name="colocalisations",
     python_requires='>3.6',
-    version="0.0.4",
+    version="0.0.5",
     author="Aurélien COLIN",
     author_email="acolin@groupcls.com",
     description="Utilities to colocate various info to Sentinel1 products",
@@ -17,6 +17,10 @@ setup(
     url="n/a",
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
+    include_package_data=True,
+    package_data={
+        'colocalisations': ['res/colormap/IR4AVHRR6.cpt', 'res/nexrad_stations.txt'],
+    },
     install_requires=requirements,
     classifiers=[
         'Programming Language :: Python :: 3.6',
