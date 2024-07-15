4ccf1463504a2c46ff80dd64f3e2ad360cde1f0a
Colin Aurélien (IMTAtlantique) <acolin@groupcls.com>
2024-07-15 10:02:45 +0200

    Default time retrieval from filename.

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # Instalation
 
 ```commandline
-pip install ...
+pip install git+https://github.com/CIA-Oceanix/Sentinel1-colocalisations.git@nokey
 ```
 
 # How to use
 
-## 
+##  
 
 export EUMETSAT_CONSUMER_KEY=...
 export EUMETSAT_CONSUMER_SECRET=...
 
 ```commandline
 python3 -m colocalisations.colocalize_seviris --pattern "/home/acolin/tmp/l2_ocn/S1A_IW_OCN__2SDV_20240528T180756_20240528T180821_054075_069332_CDE9.SAFE/measurement/*.nc"
+python3 -m colocalisations.colocalize_seviris --pattern 20161028T045448.nc --lat_key opr_lat --lon_key opr_lon
+
 ```
 
 ```commandline
--- src/colocalisations/package_utils/check_args.py
+++ src/colocalisations/package_utils/check_args.py
@@ -6,6 +6,9 @@ import netCDF4
 import numpy as np
 from dateutil import parser
 
+from . import misc
+
+
 SATELLITE_PLATFORMS = {
     'GOES': ["goes16", "goes17", "goes18"],
     'HIMAWARI': ["himawari8", "himawari9"],
@@ -33,6 +36,63 @@ def get_keys(key):
         lines = file.readlines()
         keys = [line.replace('\n', '') for line in lines]
     return keys
+
+
+def get_requests_from_nc(pattern, lat_key, lon_key, time_key, verbose):
+    requests = []
+    if lat_key is None:
+        lat_key = 'owiLat'
+    if lon_key is None:
+        lon_key = 'owiLon'
+    if time_key is None:
+        time_key = 'firstMeasurementTime'
+    filenames = glob.glob(pattern)
+    misc.log_print(f"Found {len(filenames)} files", 2, verbose)
+    for filename in filenames:
+        with netCDF4.Dataset(filename) as dataset:
+            lats = dataset[lat_key][:]
+            lons = dataset[lon_key][:]
+            try:
+                iw_datetime = parser.isoparse(getattr(dataset, time_key)).replace(tzinfo=None)
+                key = iw_datetime.strftime('%Y%m%dT%H%M%S')
+            except:
+                key = os.path.basename(os.path.splitext(filename)[0])
+                iw_datetime = datetime.strptime(key, '%Y%m%dT%H%M%S')
+
+        requests.append((key, iw_datetime, (lats, lons)))
+    return requests
+
+
+def get_requests_from_keys(sensor_operational_mode, sentinel1_keys_filename, sentinel1_key, verbose):
+    requests = []
+    misc.log_print(f"Build {sensor_operational_mode} getter", 2, verbose)
+    getter = sentinel1.getter_polygon_from_key(sensor_operational_mode)
+
+    keys = get_keys(sentinel1_keys_filename) if sentinel1_keys_filename else [sentinel1_key]
+    for key in keys:
+        res = getter(key.lower())
+        if res is None:
+            continue
+        iw_filename, iw_polygon = res[:2]
+        iw_datetime = datetime.strptime(key, '%Y%m%dt%H%M%S')
+        requests.append((key, iw_datetime, iw_polygon))
+    return requests
+
+
+def get_requests_from_txt(requests_filename):
+    requests = []
+    lines = get_keys(requests_filename)
+    for line in lines:
+        key, requested_datetime_string, lat1, lon1, lat2, lon2, lat3, lon3, lat4, lon4 = line.split('\t')
+        requested_datetime = datetime.strptime(requested_datetime_string, '%Y%m%dt%H%M%S')
+        polygon = np.array([
+            [lon1, lat1],
+            [lon2, lat2],
+            [lon3, lat3],
+            [lon4, lat4],
+        ]).astype('float')
+        requests.append((key, requested_datetime, polygon))
+    return requests
 
 
 def check_args(
@@ -77,46 +137,11 @@ def check_args(
         channel = CHANNELS[data]
 
     # Format the requests
-    requests = []
     if pattern is not None:
-        if lat_key is None:
-            lat_key = 'owiLat'
-        if lon_key is None:
-            lon_key = 'owiLon'
-        if time_key is None:
-            time_key = 'firstMeasurementTime'
-        filenames = glob.glob(pattern)
-        misc.log_print(f"Found {len(filenames)} files", 2, verbose)
-        for filename in filenames:
-            with netCDF4.Dataset(filename) as dataset:
-                lats = dataset[lat_key][:]
-                lons = dataset[lon_key][:]
-                iw_datetime = parser.isoparse(getattr(dataset, time_key)).replace(tzinfo=None)
-            key = iw_datetime.strftime('%Y%m%dT%H%M%S')
-            requests.append((key, iw_datetime, (lats, lons)))
+        requests = get_requests_from_nc(pattern, lat_key, lon_key, time_key, verbose)
     elif sentinel1_key or sentinel1_keys_filename:
-        misc.log_print(f"Build {sensor_operational_mode} getter", 2, verbose)
-        getter = sentinel1.getter_polygon_from_key(sensor_operational_mode)
-
-        keys = get_keys(sentinel1_keys_filename) if sentinel1_keys_filename else [sentinel1_key]
-        for key in keys:
-            res = getter(key.lower())
-            if res is None:
-                continue
-            iw_filename, iw_polygon = res[:2]
-            iw_datetime = datetime.strptime(key, '%Y%m%dt%H%M%S')
-            requests.append((key, iw_datetime, iw_polygon))
+        requests = get_requests_from_keys(sensor_operational_mode, sentinel1_keys_filename, sentinel1_key, verbose)
     else:
-        lines = get_keys(requests_filename)
-        for line in lines:
-            key, requested_datetime_string, lat1, lon1, lat2, lon2, lat3, lon3, lat4, lon4 = line.split('\t')
-            requested_datetime = datetime.strptime(requested_datetime_string, '%Y%m%dt%H%M%S')
-            polygon = np.array([
-                [lon1, lat1],
-                [lon2, lat2],
-                [lon3, lat3],
-                [lon4, lat4],
-            ]).astype('float')
-            requests.append((key, requested_datetime, polygon))
+        requests = get_requests_from_txt(requests_filename)
 
     return requests, channel, verbose, platforms, create_gif, max_timedelta, time_step, delta_factor
